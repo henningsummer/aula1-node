7df3c5911ebe566098e7388a0a57b974fc33c538
Hannah Summer <hannahlaisa@gmail.com>
2022-08-11 18:18:31 -0300

    refatorando modulo calculadora

---
+++ calculadora2/calcular.js
@@ -0,0 +1,6 @@
+import{soma, subtrair, dividir, multiplicar}from './operacoes.js';
+
+console.log(soma(1, 2));
+console.log(subtrair(3, 4));
+console.log(multiplicar(5, 6));
+console.log(dividir(7, 8));
---
+++ calculadora2/operacoes.js
@@ -0,0 +1,13 @@
+function soma(a, b) {
+    return a + b;
+}
+function subtrair(a, b) {
+    return a + b;
+}
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+function dividir(num1, num2) {
+    return num1 / num2;
+}
+export { soma, subtrair, multiplicar, dividir };
